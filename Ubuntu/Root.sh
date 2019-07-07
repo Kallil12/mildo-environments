6db90801e5c2db2d35093699c840ce2517e58670
Romildo Oliveira Souza Júnior <contact@mildo.me>
2019-07-07 13:11:35 -0300

    Ubuntu: software: remove GTK from Sublime Text installation

--- Ubuntu/Root.sh
+++ Ubuntu/Root.sh
@@ -36,7 +36,6 @@ install_docker_compose() {
 }
 
 install_sublime_text() {
-  apt-get install -y libgtk2.0
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
   apt-get install -y apt-transport-https
   echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
